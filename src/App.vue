17007e93f78d2280501d214eaa74d2a5352dfd39
juanpablovaz <juanpablodealmeidavaz2002@gmail.com>
2025-07-02 23:09:38 -0300

    Configurando o css da view de pedidos e home

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <Navbar/>
+  <Navbar :logo="logo_src" :alt="app_name" />
   <router-view/>
   <Footer/>
 </template>
@@ -12,10 +12,37 @@ export default {
   components:{
     Navbar,
     Footer
+  },
+  data(){
+    return {
+      logo_src: "/img/logo.png",
+      app_name: "Make Your Burguer"
+    }
   }
 }
+
 </script>
 
-<style>
+<style >
+
+*{
+  font-family: Helvetica;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.main-container {
+  margin: 50px;
+  min-height: 250px;
+}
+
+h1 {
+  text-align: center;
+  font-size: 42px;
+  margin-bottom: 30px;
+  color: #222;
+}
+
 
 </style>
